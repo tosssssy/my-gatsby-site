761b7be2a2df19859ae3a55eacff6b917e93391b
Toshiki <t.kabu.03@gmail.com>
2021-07-25 10:22:57 +0900

    build sample

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,7 +7,7 @@ import Seo from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
+    <h1>Hi from the 2 page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
