d02693accc2a828e4cc68367316ffd693e597e09
Toshiki <t.kabu.03@gmail.com>
2021-10-12 15:31:48 +0900

    hubspotコメントアウト

--- src/pages/hubspot.js
+++ src/pages/hubspot.js
@@ -6,10 +6,10 @@ import { chakra } from "@chakra-ui/react"
 const HubSpot = () => (
   <Layout>
     <chakra.div w={"50%"}>
-      <HubspotForm
+      {/* <HubspotForm
         portalId="8564937"
         formId="3c1d082b-da5f-4e86-9554-cd9b82470408"
-      />
+      /> */}
     </chakra.div>
   </Layout>
 )
